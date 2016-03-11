aac44a6aa76bee8168f97efa15e872702a9519fe
cnilin <cnilinking@gmail.com>
2016-03-11 13:50:32 +0800

    2016_3_11_13:50

---
+++ set_4.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<set>
+
+using namespace std;
+
+struct XX
+{
+    int a,b;
+};
+bool operator <(const XX &x,const XX &y)
+{
+     return x.a<y.a;
+}
+
+
+int main()
+{
+    multiset<XX> S;
+    for(int i=0;i<7;i++)
+    {
+            XX ss;
+            scanf("%d %d",&ss.a,&ss.b);
+            S.insert(ss);
+    }
+    printf("=======================\n");
+    multiset<XX>::iterator gogo;
+    for(gogo=S.begin();gogo!=S.end();gogo++)
+    {
+        printf("%d %d \n",gogo->a,gogo->b);
+    }
+
+    return 0;
+}
